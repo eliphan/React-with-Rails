6c8c8eceb61e16440dab150cef62ae836702b169
eliphan <aliceconnic@gmail.com>
2019-05-24 19:14:54 -0700

    remove build in gitignore

---
+++ client/build/precache-manifest.55075be09e12d9a8a8bb7b6c7bea12fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8974cf0c49bc805d653d68db676ab200",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0e7722be0c96c51b7e",
+    "url": "/static/css/main.c3946cef.chunk.css"
+  },
+  {
+    "revision": "5382e3f607ada76f74ff",
+    "url": "/static/js/2.61d0935a.chunk.js"
+  },
+  {
+    "revision": "4b0e7722be0c96c51b7e",
+    "url": "/static/js/main.cd49abdd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
